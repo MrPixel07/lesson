11f83cd75a7db80b14a1729feed6c7bbf3fc5f1a
MrPixel07 <pluschevr@gmail.com>
2022-11-09 15:31:38 +0300

    init project

---
+++ training.js
@@ -0,0 +1,75 @@
+// Функции -блок кода который можно выполнять многократно
+// let - можно менять многократно значение переменой
+//const - изменять переменую обьявленую как const нельзя минять
+// Функции могут быть (именованной,анонимной,присвоена переменой,аргументом при вызове,значением свойст ))
+
+//"use strict"; строгий режим
+//snake_case - формат записи переменых
+//UPPER_SNAKE_CASE
+//leftBorderWidth
+//_apiKey правило не изменять такие переменые
+//Kebab-case
+//PascalCase классы обычно называются[]
+
+//let number = 5;
+//const leftBorderWidth = 1;
+
+//number = 10;
+//console.log(number);
+
+//alert("Hello");
+
+//const result = confirm("Are you here?");
+//const answer = prompt("Вам есть 18?", "");
+//console.log(typeof(answer));
+
+//const answer = [];
+
+//answer[0] = prompt("как ваше имя?", "");
+//answer[1] = prompt("как ваше фамилия?", "");
+//answer[2] = prompt("сколько вам лет?", "");
+
+//document.write(answers);
+//        14 урок Интерполяция
+//const category = 'toys'
+//console.log(`https://www.youtube.com/${category}`)
+
+//const user = 'Ivan'
+//alert (`Hi, ${user}`)
+
+//         15 урок операторы в JS
+
+//let incr = 10,
+//    decr= 10;
+//    ++incr; форма записи перед называеться префексной
+//    decr--;              после постфиксной
+//    console.log(incr);
+//    console.log(decr);
+
+//&& - и
+//|| - или
+//- оператор отрицания
+
+//const isChecked = true,
+//  isClose = true;
+//console.log(isChecked && isClose);
+
+//const isChecked = true,
+//  isClose = false;
+//console.log(isChecked || isClose);
+//let a = 5;
+//let b = 5;
+//function sum(a, b) {
+//  const c = a + b;
+//  console.log(c);
+//}
+//sum(a, b);
+
+//a = 10;
+//b = 40;
+//sum(a, b);
+//let a = 5;
+//let b = 3;
+//let c;
+//c = a + b;
+//console.log(c);
